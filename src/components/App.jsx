4ee5a7cd956665310461ce7e158ef90c1cb9e150
TheJPMZ <monzon.jpmz@gmail.com>
2022-06-02 15:38:55 -0600

    ➕ Page Container

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import PageContainer from "./PageContainer";
 
 function App() {
     return (
         <>
             <h1>Hello World</h1>
+            <PageContainer nombre={"meme"} descripcion={"Memes Buenos"} />
         </>
     );
 }
